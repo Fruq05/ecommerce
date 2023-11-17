bbe0bc10253249ba1bed8b0833438bc9c754ab91
Faruq <faruqfolaranmi@gmail.com>
2023-11-17 22:54:28 +0100

    my first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -202,6 +202,22 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className='famous-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-3'>
+              <div className='famous-card position-relative'>
+                <img src='images/famous1.jpeg' alt='famous' className='img-famous'/>
+                <div className='famous-content position-absolute'>
+                <h5>Big Screen</h5>
+                <h6>Smart Watch Series 7</h6>
+                <p>From $399 or $16.62/mo. for 24 mo</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className='special-wrapper py-5 home-wrapper-2'>
         <div className='container-xxl'>
           <div className='row'>
@@ -216,13 +232,17 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className='feautured-wrapper py-5 home-wrapper-2'>
+      <section className='popular-wrapper py-5 home-wrapper-2'>
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-12'>
-              <h3 className='section-heading'>Featured Collection</h3>
+              <h3 className='section-heading'>Our Popular Products</h3>
             </div>
-           <ProductCard />
+           
+          </div>
+          <div className='row'>
+           
+          <ProductCard />
            <ProductCard />
            <ProductCard />
            <ProductCard />
